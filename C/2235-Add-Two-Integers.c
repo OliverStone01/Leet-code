4cfc7da00420ba7ba13d3c4d2d4f23fb27e06d36
Oliver Stone <oliverstone001@gmail.com>
2025-07-08 19:06:31 +0100

    Complete leet code 2235

---
+++ C/2235-Add-Two-Integers.c
@@ -0,0 +1,18 @@
+// Add Two Intergers
+//
+// Given two integers (num1) and (num2),
+// return the sum of the two integers.
+
+// My pseudocode:
+// return (number 1 + number 2)
+
+// My solution:
+
+class Solution(object):
+    def sum(self, num1, num2):
+        """
+        :type num1: int
+        :type num2: int
+        :rtype: int
+        """
+        return(num1 + num2)
